c90c171e41ffcb4cc538bd5272cb011bee332e06
giacThePhantom <giacomo.fantoni@studenti.unitn.it>
2020-11-06 17:31:22 +0100

    Reading files from biomart

--- Ensembl_communication/db_init.js
+++ Ensembl_communication/db_init.js
@@ -8,6 +8,21 @@ const GENE_LIST_LOCATION = './Gene_List/'
 
 
 
+function get_list_gene(file_name){
+	let file_data;
+	try{
+		file_data = fs.readFileSync(GENE_LIST_LOCATION + file_name, 'utf-8');
+	} catch(err) {
+		console.error(err);
+	}
+	if(file_data){
+		console.log('read correctly');
+		file_data = file_data.split('\n');
+		file_data.shift();
+		console.log(file_data);
+	}
+	return file_data;
+}
 
 
 
@@ -17,4 +32,12 @@ async function ensembl_get(content){
 	return await got(ENSEMBL_API + content + FORMAT_JSON);
 }
 
+
+/***************************TESTS**********************************/
+
 //ensembl_get('archive/id/ENSG00000210049').then((ret) => {console.log(ret.body)});
+
+
+
+
+//get_list_gene('mart_export_human.txt');
