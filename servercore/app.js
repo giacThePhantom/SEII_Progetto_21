b69ceac444ed1f8de6f7e4012567faeccbfeb72a
Elisa <elisa.pettina@studenti.unitn.it>
2020-11-10 17:46:17 +0100

    API: entire list of genes for species

--- servercore/app.js
+++ servercore/app.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-var GENE_LIST_LOCATION= 'C:\Users\Elisa\Desktop\progettoSE\SEII_Progetto_21\servercore\Jsons';
+var GENE_LIST_LOCATION= 'C:\\Users\\Elisa\\Desktop\\progettoSE\\SEII_Progetto_21\\servercore\\Jsons\\';
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
@@ -13,41 +13,43 @@ app.use('/', express.static('public'));
 // starting the server
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
-  console.log('Products server listening at http://localhost:' + port);  
+  console.log('Gene server listening at http://localhost:' + port);  
 });
 
-function getidgene (file_data){
+/*function getidgene (file_data){
     var obj = JSON.parse(file_data, function (key, value){
         if (key == "id"){
             list.push(value);
         }
     });
-}
+}*/
 
 
 function get_list_gene(file_name) {
-    let file_data;
-    try {
-        file_data = fs.readFileSync(GENE_LIST_LOCATION + file_name, 'utf-8');
-     } catch (err) {
-            console.error(err);
-      }
-      if (file_data) {
-          //do shit
-      }
-    return file_data;
+  const listgenes = require(GENE_LIST_LOCATION+file_name); 
+  console.log(listgenes); 
+  return listgenes;   
 }
 
-app.get('/api/species/:id', function (req, res) {
-    var id = req.params.id;
-    var product = products.find( (p) => p.id == id );
-    if(products !== undefined) {
-      res.status(200).send(product);
+app.get('/api/singlegene/:species', function (req, res) {
+    var species = req.params.species;
+    var genes = get_list_gene(species+".json");
+    if(genes) {
+      res.status(200).send(genes);
     } else {
       res.status(404);
     }
   
   });
 
-
+  app.get('/api/species/:species', function (req, res) {
+    var species = req.params.species;
+    var genes = get_list_gene(species+".json");
+    if(genes) {
+      res.status(200).send(genes);
+    } else {
+      res.status(404);
+    }
+  
+  });
 
