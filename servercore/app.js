8af7ed1db4bf709b0a49d192e0d532eb696510e4
giacThePhantom <giacomo.fantoni@studenti.unitn.it>
2020-11-25 21:31:47 +0100

    Database communication in endpoints

--- servercore/app.js
+++ servercore/app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-var GENE_LIST_LOCATION= 'C:\\Users\\Elisa\\Desktop\\progettoSE\\SEII_Progetto_21\\Ensembl_communication\\Jsons\\';
+	var GENE_LIST_LOCATION= './Ensembl_communication/Jsons';
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
@@ -26,58 +26,44 @@ app.listen(port, function(){
   console.log('Gene server listening at http://localhost:' + port);  
 });
 
-/*function getidgene (file_data){
-    var obj = JSON.parse(file_data, function (key, value){
-        if (key == "id"){
-            list.push(value);
-        }
-    });
-}*/
 
 
-function get_list_gene(file_name) {
-  try {
-    const listgenes = require(GENE_LIST_LOCATION+file_name); 
-    return listgenes;
-  } catch (error) {
-    return null;
-  }
-    
-}
 
 app.get('/api/v1/species/:species/:identifier', function (req, res) {
   var species = req.params.species;
   var identifier = req.params.identifier;
-  var genes = get_list_gene(species+".json");
 	conn.get_gene_info_for_species(species, identifier).then((ret) => {
-		res.status(200).send(ret);
+		if(res.error){
+			res.status(404).json(ret);
+		}
+		else{
+			res.status(200).send(ret);
+		}
 	});
-  /*genes.forEach(element => { //controllo la lista dei geni e restituisco info di un singolo gene
-    if (element.id == identifier){
-      res.status(200).send(element);
-    }
-  });
-  res.status(404).json({error: 'gene not found'});*/
-
 });
 
 app.get('/api/v1/species/:species', function (req, res) {
     var species = req.params.species;
-    var genes = get_list_gene(species+".json");
-    if(genes) {
-      res.status(200).send(genes);
-    } else {
-      res.status(404).json({error: 'species not found'});
-    }
-  
-  });
+	conn.get_all_genes_for_species(species).then( (ret) => {
+		if(ret.error){
+			res.status(404).json(ret);
+		}
+		else{
+			res.status(200).send(ret);
+		}
+	});
+})
+
+
+
 
 app.get('/api/v1/species', function(req, res) {
-  var species = '{["chicken", "human", "mouse", "rat", "zebrafish"]}';
-  res.status(200).send(species);
+	conn.get_all_species().then((ret) => {
+		res.status(200).send(ret);
+	})
 });
 
 console.log('Starting db init');
-//db_init.start();
+db_init.start();
   
 
--- servercore/db_conn.js
+++ servercore/db_conn.js
@@ -6,26 +6,45 @@ const models = require('../Ensembl_communication/models.js');
 
 module.exports = {
 	get_gene_info_for_species: async (species, identifier) => {
-		let species_found = await models.species_model.findOne({'name': species });
+		let species_found = await models.species_model.findOne({'name': species }, {_id : false});
 		let res;
 		if(!species_found){
-			res = {Error : "Cannot find species selected"};
+			res = {error : "Cannot find species selected"};
 		}	
 		else{
 			if(!species_found.genes.includes(identifier)){
-				res = {Error : 'Cannot find gene in species ' + species};
+				res = {error : 'Cannot find gene in species ' + species};
 			}
 			else{
-				let gene_info = await models.genes_model.findOne({'id' : identifier});
+				let gene_info = await models.genes_model.findOne({'id' : identifier}, {_id : false});
 				if(!gene_info){
-					res = {Error : 'This gene will be uploaded shortly'};
+					res = {error : 'This gene will be uploaded shortly'};
 				}
 				console.log(gene_info);
 				res = gene_info;
 			}
 		}
 		return res;
-	}	
+	},
+	
+	get_all_genes_for_species: async (species) => {
+		let genes_found = await models.species_model.findOne({'name' : species}, {_id : false});
+		let res;
+		if(!genes_found){
+			res = {error: 'Cannot find species ' + species};
+		}
+		else{
+			res = genes_found;
+		}
+		return res;
+	},
+
+	get_all_species: async () => {
+		let species_found = await models.species_model.find({}, {name: true, _id : false});
+		return species_found;
+	}
+	
+
 
 
 
