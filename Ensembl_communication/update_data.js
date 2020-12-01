c8633e0bef5162fed253ccfed046e3cb7fdb5340
EttoreFavari <ettore.favari@unitn.it>
2020-12-01 16:00:51 +0100

    .

--- Ensembl_communication/update_data.js
+++ Ensembl_communication/update_data.js
@@ -1,6 +1,7 @@
 const conn = require('./db_conn');
 const read = require('./read_data.js');
 const write = require('./write_data.js');
+const models = require('./models')
 const util = require('util');
 const mongoose = require('mongoose');
 const API_ONLY_VER='archive/id/';
