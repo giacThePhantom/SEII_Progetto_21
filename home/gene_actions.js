24a3895d1a707bbb758ca4d838d3716e9cc8c209
giacThePhantom <giacomo.fantoni@studenti.unitn.it>
2020-12-13 17:15:34 +0100

    Gene information from gene view

--- home/gene_actions.js
+++ home/gene_actions.js
@@ -10,11 +10,20 @@ title.innerHTML=gene;
 //paragraph.innerHTML=list_info;
 
 var para = document.createElement("P");     
-req_information_gene().then((ret)=>{para.innerText=ret.json(); document.body.appendChild(para);});     
+//req_information_gene().then((ret)=>{para.innerText=ret.json(); document.body.appendChild(para);});     
+console.log('Requiring gene info');
+fetch('./api/v2/gene/' + gene).then( (ret) => {
+	console.log(ret);
+	ret.json().then((test) => {
+	console.log(test);
+	para.innerText = test.species;
+	document.body.appendChild(para);
+	});	
+	
+	
+}).catch( error => console.error('Error in fetch' + error));
 
-
-
-async function req_information_gene(){
-	return fetch('//localhost:3000/api/v2/gene/'+gene)
-   .catch( error => console.error(error) ); // If there is any error you will catch them here
-}
+//async function req_information_gene(){
+//	return fetch('//localhost:3000/api/v2/gene/'+gene)
+//   .catch( error => console.error(error) ); // If there is any error you will catch them here
+//}
