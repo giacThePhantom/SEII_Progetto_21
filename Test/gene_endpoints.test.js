65bdca14baffc897bfe9e28d80e021eb6c9abab1
Elisa <elisa.pettina@studenti.unitn.it>
2020-12-14 23:44:47 +0100

    Tests servercore - genes work

--- Test/gene_endpoints.test.js
+++ Test/gene_endpoints.test.js
@@ -1,9 +1,9 @@
-const app = require("../login/login_core.js");
+const app = require("../servercore/app");
 const fetch = require("node-fetch");
 const url = "http://localhost:3000/api/v2/gene/"
 
 
-describe('login.test', () => {
+describe('gene-api.test', () => {
     let server;
     beforeAll( () => {
         const port = process.env.PORT || 3000;
@@ -29,45 +29,55 @@ describe('login.test', () => {
     });
 
     //gene/QWERTY 
-    it("get 404 status", async()=>{
+    it("get 404 status of non existing", async()=>{
         expect.assertions(1);
         let resp = await fetch (url+"QWERTY")
                 expect(resp.status).toEqual(404);
     });
 
     //gene/ENSMUSG00000118659, ok status
-	it("get ok status", async () => {
+	it("get ok status existing gene", async () => {
 		expect.assertions(1);
 		let resp =await fetch(url+"ENSMUSG00000118659")
 			expect(resp.status).toEqual(200);
     });
 
     //gene/ENSMUSG00000118659, get field
-    it("get sequence field", async()=>{
+    it("get species field",()=>{
         expect.assertions(1);
-        let resp = await fetch (url+"ENSMUSG00000118659")
-                expect(resp.status).toContain("sequence");
+        return fetch(url+"ENSMUSG00000118659")
+                .then(r => r.json())
+                .then( data => {
+                    expect({species:data["species"]}).toEqual( {species:"mus_musculus"})
+                } )
     });
 
-
+    it("get species description",()=>{
+        expect.assertions(1);
+        return fetch(url+"ENSMUSG00000118659")
+                .then(r => r.json())
+                .then( data => {
+                    expect({biotype:data["biotype"]}).toEqual( {biotype:"lincRNA"})
+                } )
+    });
 
     //gene/ENSMUSG00000118659?format=condensed
-    it("get condensed gene", () => {
+    it("get condensed existing gene", async () => {
 		expect.assertions(1);
-		return fetch(url+"ENSMUSG00000118659"+"?format=condensed").then( r => r.json()).then(data => {
-			expect(data).toEqual("{\"id\":\"ENSMUSG00000118659\",\"species\":\"mus_musculus\",\"version\":1,\"start\":140909770,\"end\":140917459,\"biotype\":\"lincRNA\",\"chromosome\":\"3\",\"strand\":-1,\"description\":\"novel transcript","homologies\":[]}");
+		let resp = await fetch(url+"ENSMUSG00000118659"+"?format=condensed").then( r => r.json()).then(data => {
+			expect(data).toEqual( {"biotype": "lincRNA", "chromosome": "3", "description": "novel transcript", "end": 140917459, "homologies": [], "id": "ENSMUSG00000118659", "species": "mus_musculus", "start": 140909770, "strand"
+            : -1, "version": 1}
+            );
 
         })
     })
 
     //gene/nonexistinggene?format=condensed
-    it("get single gene", () => {
-		expect.assertions(1);
-		return fetch(url+"QWERTY"+"?format=condensed").then( r => r.json()).then(data => {
-			expect(data).toEqual();
-
-        })
-    })
+    it("get 404 status of nonexisting + condensed", async()=>{
+        expect.assertions(1);
+        let resp = await fetch (url+"QWERTY"+"?format=condensed")
+                expect(resp.status).toEqual(404);
+    });
 })
 
 /*
