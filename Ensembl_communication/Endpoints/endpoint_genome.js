b1378f60e508be3788be35b938c2e690f37f5739
giacThePhantom <giacomo.fantoni@studenti.unitn.it>
2020-12-15 18:25:38 +0100

    Fixed typo

--- Ensembl_communication/Endpoints/endpoint_genome.js
+++ Ensembl_communication/Endpoints/endpoint_genome.js
@@ -1,5 +1,5 @@
 const express = require('express')
-sconst router = express.Router();
+const router = express.Router();
 const conn = require('../db_endpoints.js');
 
 router.get('/compara', (req, res) => {
