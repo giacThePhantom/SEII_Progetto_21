32c0d794cef870590bbfc6f0343b74bdc71931f6
FilippoGas <filippogastaldello@gmail.com>
2020-12-15 17:14:12 +0100

    modified for heroku deplot

--- Test/qanda.test.js
+++ Test/qanda.test.js
@@ -1,76 +1,72 @@
-const app = require("../qanda/qanda_core.js");
+const { identity } = require("lodash");
 const fetch = require("node-fetch");
-const url = "http://localhost:8000/api/v1/qanda"
+const db = require("../qanda/db.js");
+const url = "https://se-2-progetto-21.herokuapp.com/api/v2/qanda"
 
 describe('qanda.test', () => {
 
-    let server;
-
-    beforeAll( () => {
-        const port = process.env.PORT || 8001;
-        
-        return new Promise( (resolve,reject) => {
-            server = app.listen(port, resolve());
-            console.log('Server listening on port ${port}');
-        });
-    });
-
-    afterAll( async (done) => {
-        console.log('Closing server');
-        await server.close();
-        done();
-    });
-
-    it("get single qanda", async () => {
-        expect.assertions(1);
-        return await fetch(url+"/?id=5fd6487d76c3d02d137eaf2c")
-            .then(r => r.json())
-            .then( data => {
-                expect(data).toEqual(
-                    {
-                        "_id": "5fd6487d76c3d02d137eaf2c",
-                        "questionAuthor": "luca",
-                        "answerAuthor": "marco",
-                        "questionText": "ei",
-                        "answerText": "oi",
-                        "self": "api/v1/qanda/5fd6487d76c3d02d137eaf2c"
-                    }
-                )
-            })
-    });
-
-    it("get all qandas", async () =>{
-        expect.assertions(1);
-        let response = await fetch (url)
-            expect(response.status).toEqual(200);
-        return response;
-    });
+    var qanda_id;
 
     it("post a qanda", async () =>{
         expect.assertions(1);
+        jest.setTimeout(1200000);
         var response = await fetch(url+"/",
             {
                 method : 'POST',
                 body : JSON.stringify(
                     {
-                        "questionAuthor" : "uno",
-                        "answerAuthor" : "due",
-                        "questionText" : "tre",
-                        "answerText" : "quattro"
+                        "questionAuthor" : "autoreuno",
+                        "answerAuthor" : "autoredue",
+                        "questionText" : "domandatre",
+                        "answerText" : "rispostaquattro"
                     }),
                 headers : 
                     {
                         'content-type' : 'application/json'
                     }
             }
-        )
+        ).then(r => r.json())
+        .then( data => {
+        expect(data).toEqual( {error: "This email is already in our database"});
+   })
         var json = await response;
+        qanda_id = json._id;
+        console.log("\n\nID: ",qanda_id);
         expect(json.status).toEqual(201);
+    });
+
+    it("get single qanda", async () => {
+        expect.assertions(1);
+        jest.setTimeout(1200000);
+        return await fetch(url+"/?id="+qanda_id)
+            .then(r => r.json())
+            .then( data => {
+                expect(data).toEqual(
+                    {
+                        "_id": qanda_id,
+                        "questionAuthor": "uno",
+                        "answerAuthor": "due",
+                        "questionText": "tre",
+                        "answerText": "quattro",
+                        "self": "api/v2/qanda/"+qanda_id
+                    }
+                )
+            }
+        )
+    });
+
+    it("get all qandas", async () =>{
+        expect.assertions(1);
+        jest.setTimeout(1200000);
+        let response = await fetch (url)
+            expect(response.status).toEqual(200);
+        return response;
     });
 
     it("delete a qanda", async () => {
         expect.assertions(1);
-        let response = await fetch(url+"/?id=5fd64acd0a3a71319f752ae5")
+        jest.setTimeout(1200000);
+        let response = await fetch(url+qanda_id)
             expect(response.status).toEqual(200);
         return response;
     });
